14bd769101be7bce922f4519c6d9a2e5bf835ee7
MehmetTufan052 <tufaneser8@gmail.com>
2025-11-24 22:23:01 +0300

    Add project files.

---
+++ MvcStok/Controllers/MusteriController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Antlr.Runtime.Tree;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class MusteriController : Controller
+    {
+        // GET: Musteri
+        MvcDbStokEntities db= new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLMUSTERI.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniMusteri() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniMusteri(TBLMUSTERI p1)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("YeniMusteri");
+            }
+
+            db.TBLMUSTERI.Add(p1);
+            db.SaveChanges();
+            return View(p1);
+
+
+        }
+        public ActionResult Sil(int id)
+        {
+            var musteri=db.TBLMUSTERI.Find(id);
+            db.TBLMUSTERI.Remove(musteri);
+            db.SaveChanges();
+            return RedirectToAction("Index"); 
+
+        }
+        public ActionResult MusteriGetir(int id)
+        {
+            var mus = db.TBLMUSTERI.Find(id);
+            return View("MusteriGetir",mus);
+
+        }
+        public ActionResult Guncelle (TBLMUSTERI p1) 
+        {
+            var musteri2 = db.TBLMUSTERI.Find(p1.MUSTERIID);   
+                                                                /*Buradaki p1 view tarafında eşleşecek*/
+            musteri2.MUSTERIAD=p1.MUSTERIAD;
+            musteri2.MUSTERISOYAD = p1.MUSTERISOYAD;
+            db.SaveChanges();
+            return RedirectToAction ("Index");
+        }
+
+    }
+}
---
+++ MvcStok/Controllers/UrunlerController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+namespace MvcStok.Controllers
+{
+    public class UrunlerController : Controller
+    {
+        // GET: Urunler
+        MvcDbStokEntities db = new MvcDbStokEntities();           
+        public ActionResult Index()
+        {
+            var degerler = db.TBLURUNLER.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniUrun() 
+        {
+            List<SelectListItem> degerler =(from i in db.TBLKATEGORI.ToList()
+                                            select new SelectListItem
+                                            {
+                                                Text=i.KATEGORIAD,                  /*Burada degerler diye bir değişken atayıp sonrasında kategori tablosundaki verileri listeledik.Daha sonra kategori adı direkt olarak text olarak çektik sonrasında value olarak da kategorııdyi kullanarak biz hangi texti seçersek o textin kategorııdsine bakıp ona göre getirecek.*/
+                                                Value=i.KATEGORIID.ToString()       
+
+                                            }).ToList();
+            ViewBag.dgr = degerler;         /*Daha sonra burada viewbag kullanarak başka sayfaya bu degerleri taşıyabilme işlemini gerçekleştirmiş olacağız.*/
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult YeniUrun(TBLURUNLER p1)
+        {
+            var ktg=db.TBLKATEGORI.Where(m=>m.KATEGORIID==p1.TBLKATEGORI.KATEGORIID).FirstOrDefault();  /*FirstOrDefault burada kullandığımız linq komutunda kullanılan bir ifade.İlk veya default ifadeyi çağırmamıza yardımcı oluyor.*/
+            p1.TBLKATEGORI= ktg;
+            db.TBLURUNLER.Add(p1);
+            db.SaveChanges();
+            return RedirectToAction("Index");  /*Kaydettikten sonra bizi ındex sayfasına yönlendirmesini sağlamış olduk.*/
+        }
+
+        public ActionResult Sil(int id)
+        {
+            var urun = db.TBLURUNLER.Find(id);
+            db.TBLURUNLER.Remove(urun);
+            db.SaveChanges();
+            return RedirectToAction("Index");  
+
+        }
+        public ActionResult UrunGetir(int id)
+        {
+            var urun = db.TBLURUNLER.Find(id);
+            List<SelectListItem> degerler = (from i in db.TBLKATEGORI.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.KATEGORIAD,                  /*Burada degerler diye bir değişken atayıp sonrasında kategori tablosundaki verileri listeledik.Daha sonra kategori adı direkt olarak text olarak çektik sonrasında value olarak da kategorııdyi kullanarak biz hangi texti seçersek o textin kategorııdsine bakıp ona göre getirecek.*/
+                                                 Value = i.KATEGORIID.ToString()
+
+                                             }).ToList();
+            ViewBag.dgr = degerler;         /*Daha sonra burada viewbag kullanarak başka sayfaya bu degerleri taşıyabilme işlemini gerçekleştirmiş olacağız.*/
+            
+
+            return View("UrunGetir", urun);
+
+        }
+
+        public ActionResult Guncelle (TBLURUNLER p)
+        {
+            var urun = db.TBLURUNLER.Find(p.URUNID);
+            urun.URUNAD = p.URUNAD;
+            urun.MARKA=p.MARKA;
+            urun.STOK = p.STOK;
+            urun.FIYAT = p.FIYAT;
+            //urun.URUNKATEGORİ=p.URUNKATEGORİ;
+            var ktg = db.TBLKATEGORI.Where(m => m.KATEGORIID == p.TBLKATEGORI.KATEGORIID).FirstOrDefault();  /*FirstOrDefault burada kullandığımız linq komutunda kullanılan bir ifade.İlk veya default ifadeyi çağırmamıza yardımcı oluyor.*/
+            urun.URUNKATEGORİ = ktg.KATEGORIID;
+
+            db.SaveChanges();
+           
+            return RedirectToAction("Index");
+
+        }
+    }
+}
